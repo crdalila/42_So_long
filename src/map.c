8f2bb559684c633a95e71a522520291b9db63a2f
Dalila Cabrera Rios <dalcabre@c3r3s2.42urduliz.com>
2024-10-16 12:54:33 +0200

    gestión de errores funcional

--- src/map.c
+++ src/map.c
@@ -6,7 +6,7 @@
 /*   By: dalcabre <dalcabre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 10:46:52 by dalcabre          #+#    #+#             */
-/*   Updated: 2024/10/08 13:12:01 by dalcabre         ###   ########.fr       */
+/*   Updated: 2024/10/16 12:53:56 by dalcabre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	map_is_ber(char *argv)
 
 	len = ft_strlen(argv);
 	if (ft_strncmp(&argv[len - 4], ".ber", 4) != 0) //si no termina en .ber
-		ft_error("Error!\nIncorrect file type\n");
+		ft_error("Error!\nIncorrect file type");
 }
 
 
@@ -34,17 +34,17 @@ void	is_map_ok(char *argv, t_game *game)
 
 	fd = open(argv, O_RDONLY); //no usamos argv[1] porque en el main, ya le especificamos que es argv[1]
 	if (fd < 0) //si no es compatible
-		ft_error("Error!\nMap not found\n");
+		ft_error("Error!\nMap not found");
 	line = get_next_line(fd); //si es compatible, mira las líneas
 	if (!line)
-		ft_error("Error!\nEmpty map\n");
+		ft_error("Error!\nEmpty map");
 	game->map.x = ft_strlen(line) - 1;//calcula long de la línea sin el \n final y lo guarda en map.x
 	while (line != NULL)
 	{
 		if (line[ft_strlen(line) - 1] == '\n') //sustituir los \n por \0
 			line[ft_strlen(line) - 1] = '\0';
 		if (ft_strlen(line) != game->map.x)//columnas miden igual
-			ft_error("Error!\nIrregular map\n");
+			ft_error("Error!\nIrregular map");
 		game->map.y++;//para leer el resto de filas
 		temp = line;//para liberar la línea actual y que se pueda apuntar a la siguiente
 		line = get_next_line(fd);
@@ -65,7 +65,7 @@ void	ber_to_matrix(char *argv, t_game *game)
 //abrir
 	fd = open(argv, O_RDONLY);//no usamos argv[1] porque en el main, ya le especificamos que es argv[1]
 	if (fd < 0) //si no es compatible
-		ft_error("Error!\nMap not found\n");
+		ft_error("Error!\nMap not found");
 	/*Para acceder a t_game map y dentro de t_map a map
 	sizeof(char *) es para sacar el tamaño de un puntero
 	y lo multiplicamos por las filas que hemos calculaddo antes
@@ -79,7 +79,7 @@ void	ber_to_matrix(char *argv, t_game *game)
 //leer líneas
 	line = get_next_line(fd); //si es compatible, mira las líneas
 	if (!line)
-		ft_error("Error!\nEmpty map\n");
+		ft_error("Error!\nEmpty map");
 //pasar las líneas a la matriz	
 	i = 0;
 	while (i < game->map.y)
@@ -92,26 +92,21 @@ void	ber_to_matrix(char *argv, t_game *game)
 }
 
 void	check_map_walls(t_game *game)
-{   
-	int	i;
-	int	len;
+{
+	int	x;
+	int	y;
 
-//primera y última fila
-	len = ft_strlen(game->map.map[0]);//mide la primera, pero todas miden igual
-	i = 0;
-	while (i < len)
+	x = game->map.x; //número de columnas
+	y = game->map.y; //número de filas
+	while (x--) //empezamos por abajo hasta llegar a 0
 	{
-		if (game->map.map[0][i] != '1' || game->map.map[game->map.y - 1][i] != '1')//primera
-			ft_error("Error!\nYou need walls\n");
-		i++;
+		if (game->map.map[0][x] != '1' || game->map.map[y - 1][x] != '1') //verificar primera y última fila
+			ft_error("Error\nYou need first and last rows to be walls");
 	}
-//filas intermedias
-	i = 1;//desde la fila 2
-	while (i < game->map.y - 2)//hasta la penúltima
+	while (y--)
 	{
-		if (game->map.map[i][0] != '1' || game->map.map[i][len - 1] != '1')
-			ft_error("Error!\nYou need walls\n");
-		i++;
+		if (game->map.map[y][0] != '1' || game->map.map[y][game->map.x - 1] != '1') //verificar primera y última columna
+			ft_error("Error\nNot closed by wall");
 	}
 }
 
@@ -121,24 +116,24 @@ void	check_map_content(t_game *game)
 	int	y;
 
 	y = 0;
-	x = 0;
 	while (game->map.map[y] != NULL)//recorremos cada fila
 	{
+		x = 0;//reiniciamos al principio de cada fila
 		while (game->map.map[y][x])//cada columna de la fila actual
 		{
 			if (game->map.map[y][x] == 'P')
 				game->map.player = game->map.player + 1;
 			else if (game->map.map[y][x] == 'E')
-				game->map.player = game->map.exit + 1;
+				game->map.exit = game->map.exit + 1;
 			else if (game->map.map[y][x] == 'C')
-				game->map.player = game->map.exit + 1;
+				game->map.coin = game->map.coin + 1;
 			else if (game->map.map[y][x] != '1' && game->map.map[y][x] != '0'
 				&& game->map.map[y][x] != '\n')//saltos de línea por si acaso se ha quedado alguna en la matriz del .ber
-				ft_error("Error!\nInvalid map content\n");
+				ft_error("Error!\nInvalid map content");
 			x++;
 		}
 		y++;
 	}
 	if (game->map.player != 1 || game->map.exit != 1 || game->map.coin < 1)
-		ft_error("Error!\nInvalid map content\n");
+		ft_error("Error!\nYour map needs only one player, one exit and at least one coin");
 }
